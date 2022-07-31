51d8950ed2bc3f4452d89065eec4f764003a7513
gortanian <gortanian@gmail.com>
2022-07-30 19:30:15 -0700

    Complete rockPaperScissorsRound functionality
    The game is almost complete, but there is a bug. The game does not yet
    handle inproper user input.

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rockPaperScissorsGame () {
             playerNumberOfWins++;
         }
         // else, increment computerNumberOfWins
-        else {
+        else if (winner === "computer") {
             computerNumberOfWins++;
         }
 
@@ -54,11 +54,14 @@ function rockPaperScissorsGame () {
 
     // Determine who the winner is
     let winner = ''; 
-    if (computerNumberOfWins >= 3) {
+    if (computerNumberOfWins > playerNumberOfWins) {
         winner = "computer";
     }
-    else if (playerNumberOfWins >= 3) {
+    else if (playerNumberOfWins > computerNumberOfWins) {
         winner = "player";
+    }
+    else if (playerNumberOfWins === computerNumberOfWins) {
+        winner = "tie";
     }
 
     // print the winner message to the user
@@ -67,6 +70,9 @@ function rockPaperScissorsGame () {
     }
     else if (winner === "player") {
         console.log(`You won! You defeated the computer at rock paper scissors with a final score of ${playerNumberOfWins} to ${computerNumberOfWins}`);
+    }
+    else if (winner === "tie") {
+        console.log(`It's a full-on Tie! What a Match! the final score is ${playerNumberOfWins} to ${computerNumberOfWins}`);
     }
     else {
         console.log("error, there appears to be no winner.");
@@ -82,38 +88,102 @@ function rockPaperScissorsRound() {
     let userInput = prompt("choose rock, paper, or scissors (type it in)")
 
     // check if user input is either rock, paper, or scissors, and store it in user choice
-    // initialize a variable called userChoice
-    let userChoice = '';
+    // initialize a variable called playerChoice
+    let playerChoice = '';
 
     // first, check if it's rock
     if (userInput.toUpperCase() === "ROCK") {
-        userChoice = "Rock";
+        playerChoice = "Rock";
     }
 
     // then, check if it's paper
     else if (userInput.toUpperCase() === "PAPER") {
-        userChoice = "Paper";
+        playerChoice = "Paper";
     }
 
     // finally, check if it's scissors
     else if (userInput.toUpperCase() === "SCISSORS") {
-        userChoice = "Scissors";
+        playerChoice = "Scissors";
     }
 
-    // if it was none of these, set userChoice to Invalid
+    // if it was none of these, set playerChoice to Invalid
     else {
-        userChoice = "Invalid"
+        playerChoice = "Invalid"
     }
-
-    console.log("you chose " + userChoice);
-
 
     // store a random choice between rock paper and scissors for the computer
+    // first, set a variable to a random number between 1 and 3
+    let computerInt = Math.floor(Math.random() * 3) + 1;
 
-    // compare the choices, and decide the winner
+    // then, set another variable to rock, paper, or scissors, depending on computerInt
+    let computerChoice = ''; 
+    if (computerInt === 1) {
+        computerChoice = "Rock";
+    }
+    else if (computerInt === 2) {
+        computerChoice = "Paper";
+    }
+    else if (computerInt === 3) {
+        computerChoice = "Scissors";
+    }
+    else {
+        console.log("Computer choice error");
+    }
+
+    // print what the computer chose
+    console.log("the computer chose " + computerChoice);
+
+    // compare the choices, and return the winner
+     let winner = compareChoice(playerChoice, computerChoice);
 
     // print the results of the round to the console. 
+    if (winner === "player") {
+        console.log("You Win! " + playerChoice + " beats " + computerChoice + "!");
+    }
+    else if (winner === "computer") {
+        console.log("You Lose! " + computerChoice + " beats " + playerChoice + "!");
+    }
+    else if (winner === "tie") {
+        console.log("It's a Tie! you and the computer both chose " + playerChoice);
+    }
+    else {
+        console.log("round winner error. compare choice returned something funky.");
+    }
 
     // return the winner
-    return "computer";
+    return winner;
+
+    function compareChoice(playerChoice, computerChoice) {
+        let winner = 'none'
+
+        // check all possible iterations of rock paper scissors
+        if (playerChoice === computerChoice) { 
+            winner = "tie";
+        }
+
+        if (playerChoice === "Rock") {
+            if (computerChoice === "Paper") {
+                winner = "computer"; 
+            }
+            if (computerChoice === "Scissors")
+                winner = "player";
+        }    
+        if (playerChoice === "Paper") {
+            if (computerChoice === "Rock") {
+                winner = "player"; 
+            }
+            if (computerChoice === "Scissors")
+                winner = "computer";
+        }    
+        if (playerChoice === "Scissors") {
+            if (computerChoice === "Paper") {
+                winner = "player"; 
+            }
+            if (computerChoice === "Rock")
+                winner = "computer";
+        }    
+
+        return winner;
+    }
+
 }
